fde372a7246ded8240caadd1645ebd3489c1e35c
ddiego18 <ddiego18@gmail.com>
2021-07-11 19:15:01 -0300

    Subir Código

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2021,08,20);
+            DateTime dataCorrente = DateTime.Now;
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+           
+
+           
+
+            //ContaCorrente conta = new ContaCorrente(847, 489754);
+            //Console.WriteLine(conta.Numero);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank.SistemaInterno/SistemaInterno/SistemaInterno/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1014, 155320);
+            Console.WriteLine(conta.Saldo);
+
+            conta.Sacar(50.0);
+
+            Console.WriteLine(conta.Saldo);
+
+            conta.Sacar(200.0);
+
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
